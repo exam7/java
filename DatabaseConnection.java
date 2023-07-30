081a2c955f94d74f1be69a725393234b9042e3a7
exam7 <127329100+exam7@users.noreply.github.com>
2023-07-30 14:19:46 +0530

    Create DatabaseConnection.java

---
+++ DatabaseConnection.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection{
+    public static void main(String[] args) {
+        String databaseUrl = "jdbc:mysql://localhost:3306/mydatabase";
+        String username = "harshil";
+        String password = "1234";
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver"); 
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(databaseUrl, username, password);
+            System.out.println("connection estlablish");
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    } 
+}
