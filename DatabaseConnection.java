bc8eae48a47ba953746b132dfbd904b94c76ee3b
exam7 <127329100+exam7@users.noreply.github.com>
2023-07-30 14:36:51 +0530

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection{
     public static void main(String[] args) {
         String databaseUrl = "jdbc:mysql://localhost:3306/mydatabase";
-        String username = "harshil";
+        String username = "root";
         String password = "1234";
 
         try {
